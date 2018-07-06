68b825443e1ae9d8638fb1380dd66c5f5c1ed769
Enzo Cano <enzo.cano@southworks.com>
2018-07-06 18:01:16 -0300

    Add base duration extractor

--- Java/libraries/recognizers-text-date-time/src/main/java/com/microsoft/recognizers/text/datetime/extractors/config/ResultIndex.java
+++ Java/libraries/recognizers-text-date-time/src/main/java/com/microsoft/recognizers/text/datetime/extractors/config/ResultIndex.java
@@ -1,11 +1,11 @@
 package com.microsoft.recognizers.text.datetime.extractors.config;
 
 public class ResultIndex {
-    final boolean result;
-    final int index;
+  final boolean result;
+  final int index;
 
-    public ResultIndex(boolean result, int index) {
-        this.result = result;
-        this.index = index;
-    }
+  public ResultIndex(boolean result, int index) {
+    this.result = result;
+    this.index = index;
+  }
 }
