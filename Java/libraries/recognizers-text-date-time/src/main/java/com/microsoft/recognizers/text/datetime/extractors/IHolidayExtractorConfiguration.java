9725ffa113bf52f8ce1b69a418747be8cc9d1ca0
VictorGrycuk <victor@grycuk.net>
2018-11-05 08:40:47 -0300

    Remove duplicated interface

--- Java/libraries/recognizers-text-date-time/src/main/java/com/microsoft/recognizers/text/datetime/extractors/IHolidayExtractorConfiguration.java
+++
@@ -1,8 +0,0 @@
-package com.microsoft.recognizers.text.datetime.extractors;
-
-import com.microsoft.recognizers.text.datetime.config.IOptionsConfiguration;
-import java.util.regex.Pattern;
-
-public interface IHolidayExtractorConfiguration extends IOptionsConfiguration {
-    Pattern[] getHolidayRegexes();
-}
