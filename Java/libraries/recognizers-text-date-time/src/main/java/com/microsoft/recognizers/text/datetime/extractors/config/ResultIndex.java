bf88f4109f756d16b1551dfcac390f0900a3f72c
Enzo Cano <enzo.cano@southworks.com>
2018-11-06 16:10:48 -0300

    Java datetime extractor dateperiod (#14)
    * BaseDatePeriodExtractor and English configuration
    
    * Enable DatePeriodExtractor tests
    
    * Fix DatePeriod extractor

--- Java/libraries/recognizers-text-date-time/src/main/java/com/microsoft/recognizers/text/datetime/extractors/config/ResultIndex.java
+++ Java/libraries/recognizers-text-date-time/src/main/java/com/microsoft/recognizers/text/datetime/extractors/config/ResultIndex.java
@@ -1,8 +1,8 @@
 package com.microsoft.recognizers.text.datetime.extractors.config;
 
 public class ResultIndex {
-  final boolean result;
-  final int index;
+  public final boolean result;
+  public final int index;
 
   public ResultIndex(boolean result, int index) {
     this.result = result;
