a3f2163b96475af142ce19f6b7e5f8fcc7e1e098
David Favretto <david.favretto@southworks.com>
2018-11-21 16:21:29 -0300

    Finish porting DateTimeAltParser and enable its tests

--- Java/tests/src/test/java/com/microsoft/recognizers/text/tests/datetime/DateTimeParserTest.java
+++ Java/tests/src/test/java/com/microsoft/recognizers/text/tests/datetime/DateTimeParserTest.java
@@ -124,6 +124,8 @@ public class DateTimeParserTest extends AbstractTest {
 				return new BaseTimePeriodParser(new EnglishTimePeriodParserConfiguration(new EnglishCommonDateTimeParserConfiguration(DateTimeOptions.None)));
 			case "DateTimePeriodParser":
 				return new BaseDateTimePeriodParser(new EnglishDateTimePeriodParserConfiguration(new EnglishCommonDateTimeParserConfiguration(DateTimeOptions.None)));
+			case "DateTimeAltParser":
+				return new BaseDateTimeAltParser(new EnglishDateTimeAltParserConfiguration(new EnglishCommonDateTimeParserConfiguration(DateTimeOptions.None)));
 			default:
 				throw new AssumptionViolatedException("Parser Type/Name not supported.");
 		}
