8d627b0d18014cedfebd1b046d5499d771b33137
Aliandi <mica.andini@gmail.com>
2019-01-03 17:00:06 -0300

    Fix StyleCop warnings in TimexUtility

---
+++ .NET/Microsoft.Recognizers.Text.DateTime/Utilities/RangeTimexComponents.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Globalization;
+using System.Text;
+using DateObject = System.DateTime;
+
+namespace Microsoft.Recognizers.Text.DateTime
+{
+    public class RangeTimexComponents
+    {
+        public string BeginTimex;
+
+        public string EndTimex;
+
+        public string DurationTimex;
+
+        public bool IsValid = false;
+    }
+}
