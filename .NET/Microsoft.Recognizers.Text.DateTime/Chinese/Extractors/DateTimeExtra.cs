06876b3e4c94070c10e67678148e6265a344676b
Cecilia Avila <cecilia.avila@southworks.com>
2019-01-04 12:15:25 -0300

    Fix all StyleCop warnings in BaseDateTimeExtractor
    - Classes were separated into different files.
    - Reordered using statements.
    - Added spaces and trailing commas when needed.

---
+++ .NET/Microsoft.Recognizers.Text.DateTime/Chinese/Extractors/DateTimeExtra.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Microsoft.Recognizers.Text.DateTime.Chinese
+{
+    public class DateTimeExtra<T>
+    {
+        public GroupCollection NamedEntity { get; set; }
+
+        public T Type { get; set; }
+    }
+}
