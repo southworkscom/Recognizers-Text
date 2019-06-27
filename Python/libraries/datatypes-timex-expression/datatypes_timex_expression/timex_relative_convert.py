2f9ced958bf57ab6d9e8ba8f1c69c721c9fb84e9
aliandi <mica.andini@gmail.com>
2019-06-27 15:47:53 -0300

    relative convert

--- Python/libraries/datatypes-timex-expression/datatypes_timex_expression/timex_relative_convert.py
+++ Python/libraries/datatypes-timex-expression/datatypes_timex_expression/timex_relative_convert.py
@@ -1,5 +1,8 @@
+from datatypes_timex_expression.english import convert_timex_to_string_relative as english_convert_timex_to_string_relative
+
+
 class TimexRelativeConvert:
 
     @staticmethod
-    def convert_timex_to_string_relative(timex):
-        return ''
+    def convert_timex_to_string_relative(timex, reference_date):
+        return english_convert_timex_to_string_relative(timex, reference_date)
