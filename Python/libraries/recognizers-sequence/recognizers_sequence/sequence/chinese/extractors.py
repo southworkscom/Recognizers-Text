81505314cbbdf45aa0ca2af9d6295200df52c917
Mai <mailen.havela@southworks.com>
2019-07-17 16:07:04 -0300

    Add missing ChineseURLExtractorConfiguration class

--- Python/libraries/recognizers-sequence/recognizers_sequence/sequence/chinese/extractors.py
+++ Python/libraries/recognizers-sequence/recognizers_sequence/sequence/chinese/extractors.py
@@ -1,6 +1,7 @@
 from recognizers_sequence.resources.chinese_phone_numbers import ChinesePhoneNumbers
 from recognizers_sequence.sequence.extractors import *
 from recognizers_text.culture import Culture
+from recognizers_sequence.resources import BasePhoneNumbers
 import regex as re
 
 
@@ -24,3 +25,20 @@ class ChinesePhoneNumberExtractorConfiguration(BaseSequenceExtractorConfiguratio
         self._WordBoundariesRegex = ChinesePhoneNumbers.WordBoundariesRegex
         self._NonWordBoundariesRegex = ChinesePhoneNumbers.NonWordBoundariesRegex
         self._EndWordBoundariesRegex = ChinesePhoneNumbers.EndWordBoundariesRegex
+
+class ChineseURLExtractorConfiguration(BaseSequenceExtractorConfiguration):
+    @property
+    def url_regex(self) -> str:
+        return self._UrlRegex
+
+    @property
+    def ip_url_regex(self) -> str:
+        return self._IpUrlRegex
+
+    def __init__(self, culture_info: CultureInfo = None):
+        if culture_info is None:
+            culture_info = CultureInfo(Culture.Chinese)
+        super().__init__(culture_info)
+
+        self._UrlRegex = RegExpUtility.get_safe_reg_exp(ChinesePhoneNumbers.UrlRegex)
+        self._IpUrlRegex = RegExpUtility.get_safe_reg_exp(ChinesePhoneNumbers.IpUrlRegex)
