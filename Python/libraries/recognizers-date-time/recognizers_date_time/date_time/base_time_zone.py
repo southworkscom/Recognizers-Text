d4f0e218499ac55c48220bf8533cec9562ec7819
Mai <mailen.havela@southworks.com>
2019-12-09 17:16:41 -0300

    WIP: Add compute_minutes implementation

--- Python/libraries/recognizers-date-time/recognizers_date_time/date_time/base_time_zone.py
+++ Python/libraries/recognizers-date-time/recognizers_date_time/date_time/base_time_zone.py
@@ -1,3 +1,4 @@
+import time
 from typing import List
 from datetime import datetime
 from .parsers import DateTimeParser, DateTimeParseResult
@@ -11,8 +12,36 @@ class BaseTimeZoneParser(DateTimeParser):
     def parser_name(self) -> str:
         return Constants.SYS_DATETIME_TIMEZONE
 
-    def compute_minutes(self, utc_offset: str) -> int:
-        pass
+    @staticmethod
+    def compute_minutes(utc_offset: str) -> int:
+        if len(utc_offset) == 0:
+            return Constants.INVALID_OFFSET_VALUE
+
+        utc_offset = utc_offset.strip()
+
+        sign = Constants.POSITIVE_SIGN  # later than utc, default value
+        if utc_offset.startswith("+") or utc_offset.startswith("-") or utc_offset.startswith("±"):
+            if utc_offset.startswith("-"):
+                sign = Constants.NEGATIVE_SIGN  # Earlier than utc 0
+
+        hour, minutes = 0
+        if utc_offset.contains("+"):
+            tokens = list(utc_offset.split(":"))
+            hour = int(tokens[0])
+            minutes = int(tokens[1])
+        elif time.strptime(utc_offset):
+            minutes = 0
+
+        if hour > Constants.HALF_DAY_HOUR_COUNT:
+            return Constants.INVALID_OFFSET_VALUE
+
+        if minutes != 0 and minutes != 15 and minutes != 30 and minutes != 45 and minutes != 60:
+            return Constants.INVALID_OFFSET_VALUE
+
+        offset_in_minutes = (hour * 60) + minutes
+        offset_in_minutes *= sign
+
+        return offset_in_minutes
 
     def convert_offset_in_mins_to_offset_string(self, offset_mins: int) -> str:
         pass
