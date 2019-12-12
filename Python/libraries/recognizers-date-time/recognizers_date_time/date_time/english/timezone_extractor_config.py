29c55f64d26a10a395b0ecbac29e36fde751435d
Mai <mailen.havela@southworks.com>
2019-12-12 13:41:22 -0300

    WIP: Add EnglishDateExtractorConfiguration constructor & initialize props

--- Python/libraries/recognizers-date-time/recognizers_date_time/date_time/english/timezone_extractor_config.py
+++ Python/libraries/recognizers-date-time/recognizers_date_time/date_time/english/timezone_extractor_config.py
@@ -1,6 +1,9 @@
 from typing import List, Pattern
 from recognizers_text.matcher.string_matcher import StringMatcher
 from ..base_timezone import TimeZoneExtractorConfiguration
+from recognizers_text.utilities import RegExpUtility
+from recognizers_date_time.resources.english_time_zone import TimeZoneDefinitions
+from ..utilities import DateTimeOptions
 
 
 class EnglishDateExtractorConfiguration(TimeZoneExtractorConfiguration):
@@ -31,3 +34,22 @@ class EnglishDateExtractorConfiguration(TimeZoneExtractorConfiguration):
     @property
     def ambiguous_timezone_list(self) -> List[str]:
         return self._ambiguous_timezone_list
+
+    def __init__(self):
+        options: DateTimeOptions = DateTimeOptions.NONE,
+        if options and DateTimeOptions.ENABLE_PREVIEW != 0:
+            self.location_matcher.init()
+
+        self._direct_utc_regex = RegExpUtility.get_safe_reg_exp(
+            TimeZoneDefinitions.DirectUtcRegex,
+            flags=self.direct_utc_regex.IGNORECASE or self.direct_utc_regex.MULTILINE)
+        self._abbreviations_list = RegExpUtility.get_safe_reg_exp(
+            TimeZoneDefinitions.AbbreviationsList)
+        self._full_name_list = RegExpUtility.get_safe_reg_exp(
+            TimeZoneDefinitions.FullNameList)
+        self._location_time_suffix_regex = RegExpUtility.get_safe_reg_exp(
+            TimeZoneDefinitions.LocationTimeSuffixRegex,
+            flags=self.direct_utc_regex.IGNORECASE or self.direct_utc_regex.MULTILINE)
+        self._location_matcher = StringMatcher()
+        self._ambiguous_timezone_list = RegExpUtility.get_safe_reg_exp(
+            list(TimeZoneDefinitions.AmbiguousTimezoneList))
