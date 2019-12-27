0ec7749aeb159f5a1dd826621ff17211e4ee484f
Giuliano Taliano <giuliano.taliano@southworks.com>
2019-12-27 14:06:13 -0300

    Minor fix

--- Python/libraries/recognizers-date-time/recognizers_date_time/date_time/base_timezone.py
+++ Python/libraries/recognizers-date-time/recognizers_date_time/date_time/base_timezone.py
@@ -119,7 +119,7 @@ class BaseTimeZoneParser(DateTimeParser):
             utc_minute_shift = TimeZoneDefinitions.AbbrToMinMapping[normalized_text]
 
             datetime_result.value = self.get_datetime_resolution_result(utc_minute_shift, text)
-            datetime_result.resolution_str = Constants.UTC_OFFSET_MINS_KEY + ": " + utc_minute_shift
+            datetime_result.resolution_str = Constants.UTC_OFFSET_MINS_KEY + ": " + str(utc_minute_shift)
         elif normalized_text in TimeZoneDefinitions.FullToMinMapping and TimeZoneDefinitions.FullToMinMapping[normalized_text] != Constants.INVALID_OFFSET_VALUE:
             utc_minute_shift = TimeZoneDefinitions.FullToMinMapping[normalized_text]
 
