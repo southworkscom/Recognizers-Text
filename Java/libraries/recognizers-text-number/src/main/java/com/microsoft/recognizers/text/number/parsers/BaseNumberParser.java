9cd5ac0471762ab2a8dcce502e6be7ed4f848ea1
Federico Bernal <64086728+FedericoBernal@users.noreply.github.com>
2021-05-29 13:27:27 -0300

    Improve the validation in the Number model
    * Improve number model validations
    
    * Add java as supported type for ignored NumberRangeModels
    
    * Add Java as non-supported model as BaseMergedNumberExtractor doesnt exist
    
    * Accept TwoNumClosed type
    
    * Support for RangeNumberExperimentalMode
    
    * Separate call methods depending on NumberOptions type
    
    * Remove Unit validation and use getKeysToTest methods
    
    * Accept metadata object in Numbers
    
    * Set NumberOptions.None for PercentModel type tests
    
    * Improve parity against C#
    
    Co-authored-by: Martin Battaglino <martinbatta32@gmail.com>

--- Java/libraries/recognizers-text-number/src/main/java/com/microsoft/recognizers/text/number/parsers/BaseNumberParser.java
+++ Java/libraries/recognizers-text-number/src/main/java/com/microsoft/recognizers/text/number/parsers/BaseNumberParser.java
@@ -3,9 +3,11 @@ package com.microsoft.recognizers.text.number.parsers;
 import com.microsoft.recognizers.text.CultureInfo;
 import com.microsoft.recognizers.text.ExtractResult;
 import com.microsoft.recognizers.text.IParser;
+import com.microsoft.recognizers.text.Metadata;
 import com.microsoft.recognizers.text.ParseResult;
 import com.microsoft.recognizers.text.number.Constants;
 import com.microsoft.recognizers.text.utilities.QueryProcessor;
+import org.apache.commons.lang3.StringUtils;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -116,6 +118,13 @@ public class BaseNumberParser implements IParser {
             ret.setResolutionStr(resolutionStr);
         }
 
+        // Add "offset" and "relativeTo" for ordinal
+        if (StringUtils.isNotBlank(ret.getType()) && ret.getType().contains(Constants.MODEL_ORDINAL)) {
+            ret.getMetadata().setOffset(ret.getResolutionStr());
+            // Every ordinal number is relative to the start
+            ret.getMetadata().setRelativeTo(Constants.RELATIVE_START);
+        }
+
         if (ret != null) {
             ret.setText(ret.getText().toLowerCase(Locale.ROOT));
         }
@@ -133,6 +142,8 @@ public class BaseNumberParser implements IParser {
 
         ParseResult result = new ParseResult(extractResult.getStart(), extractResult.getLength(), extractResult.getText(),
                 extractResult.getType(),null,null,null);
+        Metadata metadata = extractResult.getMetadata() != null ? extractResult.getMetadata() : new Metadata();
+        result.setMetadata(metadata);
 
         //[1] 24
         //[2] 12 32/33
@@ -298,6 +309,7 @@ public class BaseNumberParser implements IParser {
     private ParseResult textNumberParse(ExtractResult extractResult) {
 
         ParseResult result = new ParseResult(extractResult.getStart(), extractResult.getLength(), extractResult.getText(), extractResult.getType(), null, null, null);
+        result.setMetadata(extractResult.getMetadata());
         String handle = extractResult.getText().toLowerCase();
 
         //region Special case for "dozen"
